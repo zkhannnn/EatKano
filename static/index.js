8b6b9c7fc998926941cf1c856911a374d210a199
zkhannnn <113614162+zkhannnn@users.noreply.github.com>
2022-09-16 10:01:53 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -185,15 +185,15 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
 
     function gameInit() {
         createjs.Sound.registerSound({
-            src: "./static/music/err.mp3",
+            src: "./static/music/迎新楼 2.mp3",
             id: "err"
         });
         createjs.Sound.registerSound({
-            src: "./static/music/end.mp3",
+            src: "./static/music/迎新楼.mp3",
             id: "end"
         });
         createjs.Sound.registerSound({
-            src: "./static/music/tap.mp3",
+            src: "./static/music/世康口腔诊所.mp3",
             id: "tap"
         });
         gameRestart();
